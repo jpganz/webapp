ee0a9dc49af42b6d7e50211fdfcfdf0cbe66ef8e
Juan Lopez <jlopez@sigel.gt>
2017-07-14 03:41:16 -0600

    view admin

---
+++ src/app/pages/admin/admin.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by juanlopez on 13/07/17.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin',
+  styleUrls: ['./admin.scss'],
+  templateUrl: './admin.html'
+})
+export class Admin {
+
+  constructor() {
+  }
+
+}
