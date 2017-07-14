8855836490750b4984f9395535b8427a141d482a
Juan Lopez <jlopez@sigel.gt>
2017-07-13 22:31:33 -0600

    order code and delete files

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -21,12 +21,12 @@ export const routes: Routes = [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
       { path: 'editors', loadChildren: './editors/editors.module#EditorsModule' },
-      { path: 'components', loadChildren: './components/components.module#ComponentsModule' },
-      { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-      { path: 'ui', loadChildren: './ui/ui.module#UiModule' },
-      { path: 'forms', loadChildren: './forms/forms.module#FormsModule' },
-      { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-      { path: 'maps', loadChildren: './maps/maps.module#MapsModule' }
+      // { path: 'components', loadChildren: './components/components.module#ComponentsModule' },
+      // { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
+      // { path: 'ui', loadChildren: './ui/ui.module#UiModule' },
+      // { path: 'forms', loadChildren: './forms/forms.module#FormsModule' },
+      // { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
+      // { path: 'maps', loadChildren: './maps/maps.module#MapsModule' }
     ]
   }
 ];
